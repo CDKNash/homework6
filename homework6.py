4b1a8ca7359a70e9c0a88c658e53ed7c73d5cf1d
CDKNash <cdknash123@gmail.com>
2024-07-01 21:10:38 +0400

    Update homework6.py

--- homework6.py
+++ homework6.py
@@ -1,13 +1,10 @@
 my_dict = {'Ivan': 1990, 'Masha': 1995, 'Igor': 1993, 'Ruslan': 1992}
 print(my_dict)
-print(my_dict['Igor'])
-my_dict['Igor'] = 2000
-print(my_dict['Igor'])
-print(my_dict)
-my_dict['Dima'] = 2002
-my_dict['Lena'] = 2005
-print(my_dict)
-del my_dict['Ivan']
+print(my_dict.get('Igor'))
+print(my_dict.get('Vova', 'Такого имени нет'))
+my_dict.update({'Dima': 2002,
+                'Lena': 2005})
+my_dict.pop('Ruslan')
 print(my_dict)
 
 my_set = {1, 10, 100, 1000, 1, 10, 100, 1000}
